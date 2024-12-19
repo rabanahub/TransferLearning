298787993d233335b81677e3d78a4da38df26f59
rabanahub <rabanamulla@gmail.com>
2024-12-19 20:25:17 +0530

    Add files via upload

---
+++ Resnet_50_Transfer_learning.py
@@ -0,0 +1,71 @@
+from sklearn.datasets import load_files
+from keras.utils import np_utils
+import numpy as np
+from glob import glob
+
+# define function to load train, test, and validation datasets
+def load_dataset(path):
+    data = load_files(path)
+    dog_files = np.array(data['filenames'])
+    dog_targets = np_utils.to_categorical(np.array(data['target']), 133)
+    return dog_files, dog_targets
+
+# load train, test, and validation datasets
+train_files, train_targets = load_dataset('D:\\Trainig Material\\Deep_Learning_Praticals\\Transfer_learning\\data\\train')
+valid_files, valid_targets = load_dataset('D:\\Trainig Material\\Deep_Learning_Praticals\\Transfer_learning\\data\\valid')
+test_files, test_targets = load_dataset('D:\\Trainig Material\\Deep_Learning_Praticals\\Transfer_learning\\data\\test')
+
+# load list of dog names
+dog_names = [item[20:-1] for item in sorted(glob("D:\\Trainig Material\\Deep_Learning_Praticals\\Transfer_learning\\data\\train\\*\\"))]
+
+# print statistics about the dataset
+print('There are %d total dog categories.' % len(dog_names))
+print('There are %s total dog images.\n' % len(np.hstack([train_files, valid_files, test_files])))
+print('There are %d training dog images.' % len(train_files))
+print('There are %d validation dog images.' % len(valid_files))
+print('There are %d test dog images.'% len(test_files))
+
+
+
+from tensorflow.keras.applications.resnet50 import ResNet50
+
+# define ResNet50 model
+ResNet50_model = ResNet50(weights='imagenet')
+
+dog_files_short = train_files[:100]
+
+from keras.preprocessing import image
+from tqdm import tqdm
+
+def path_to_tensor(img_path):
+    # loads RGB image as PIL.Image.Image type
+    img = image.load_img(img_path, target_size=(224, 224))
+    # convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
+    x = image.img_to_array(img)
+    # convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
+    return np.expand_dims(x, axis=0)
+
+def paths_to_tensor(img_paths):
+    list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
+    return np.vstack(list_of_tensors)
+
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+
+def ResNet50_predict_labels(img_path):
+    # returns prediction vector for image located at img_path
+    img = preprocess_input(path_to_tensor(img_path))
+    return np.argmax(ResNet50_model.predict(img))
+
+# returns "True" if a dog is detected in the image stored at img_path
+def dog_detector(img_path):
+    prediction = ResNet50_predict_labels(img_path)
+    return ((prediction <= 268) & (prediction >= 151))
+
+
+def percent_dog(file_array):
+    is_dog = [ dog_detector(file) for file in file_array ]
+    return np.mean(is_dog)
+
+
+percent_dog(dog_files_short)
+
